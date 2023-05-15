a910174939353974e55aafa2856e7b4544c39e70
HJ <heeee20000@gmail.com>
2023-05-15 14:13:17 +0900

    commit

---
+++ src/main/java/hj/EmployeeVO.java
@@ -0,0 +1,188 @@
+package hj;
+
+public class EmployeeVO {
+	private int e_number;
+	private String e_name;
+	private String e_id;
+	private String e_password;
+	private String e_phone_number;
+	private int e_postal_code;
+	private String e_address1;
+	private String e_address2;
+	private String e_birth;
+	private String e_date_joining_company;
+	private String e_retirement_date;
+	private String e_account_information;
+	private String e_gender;
+	private String e_official_responsibilities;
+	private int e_total_vacation;
+	private String e_serving;
+	private String d_name;
+	
+	public EmployeeVO() {
+		
+	}
+
+	public EmployeeVO(int e_number, String e_name, String e_id, String e_password, String e_phone_number,
+			int e_postal_code, String e_address1, String e_address2, String e_birth, String e_date_joining_company,
+			String e_retirement_date, String e_account_information, String e_gender, String e_official_responsibilities,
+			int e_total_vacation, String e_serving, String d_name) {
+		super();
+		this.e_number = e_number;
+		this.e_name = e_name;
+		this.e_id = e_id;
+		this.e_password = e_password;
+		this.e_phone_number = e_phone_number;
+		this.e_postal_code = e_postal_code;
+		this.e_address1 = e_address1;
+		this.e_address2 = e_address2;
+		this.e_birth = e_birth;
+		this.e_date_joining_company = e_date_joining_company;
+		this.e_retirement_date = e_retirement_date;
+		this.e_account_information = e_account_information;
+		this.e_gender = e_gender;
+		this.e_official_responsibilities = e_official_responsibilities;
+		this.e_total_vacation = e_total_vacation;
+		this.e_serving = e_serving;
+		this.d_name = d_name;
+	}
+
+	public int getE_number() {
+		return e_number;
+	}
+
+	public void setE_number(int e_number) {
+		this.e_number = e_number;
+	}
+
+	public String getE_name() {
+		return e_name;
+	}
+
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+
+	public String getE_id() {
+		return e_id;
+	}
+
+	public void setE_id(String e_id) {
+		this.e_id = e_id;
+	}
+
+	public String getE_password() {
+		return e_password;
+	}
+
+	public void setE_password(String e_password) {
+		this.e_password = e_password;
+	}
+
+	public String getE_phone_number() {
+		return e_phone_number;
+	}
+
+	public void setE_phone_number(String e_phone_number) {
+		this.e_phone_number = e_phone_number;
+	}
+
+	public int getE_postal_code() {
+		return e_postal_code;
+	}
+
+	public void setE_postal_code(int e_postal_code) {
+		this.e_postal_code = e_postal_code;
+	}
+
+	public String getE_address1() {
+		return e_address1;
+	}
+
+	public void setE_address1(String e_address1) {
+		this.e_address1 = e_address1;
+	}
+
+	public String getE_address2() {
+		return e_address2;
+	}
+
+	public void setE_address2(String e_address2) {
+		this.e_address2 = e_address2;
+	}
+
+	public String getE_birth() {
+		return e_birth;
+	}
+
+	public void setE_birth(String e_birth) {
+		this.e_birth = e_birth;
+	}
+
+	public String getE_date_joining_company() {
+		return e_date_joining_company;
+	}
+
+	public void setE_date_joining_company(String e_date_joining_company) {
+		this.e_date_joining_company = e_date_joining_company;
+	}
+
+	public String getE_retirement_date() {
+		return e_retirement_date;
+	}
+
+	public void setE_retirement_date(String e_retirement_date) {
+		this.e_retirement_date = e_retirement_date;
+	}
+
+	public String getE_account_information() {
+		return e_account_information;
+	}
+
+	public void setE_account_information(String e_account_information) {
+		this.e_account_information = e_account_information;
+	}
+
+	public String getE_gender() {
+		return e_gender;
+	}
+
+	public void setE_gender(String e_gender) {
+		this.e_gender = e_gender;
+	}
+
+	public String getE_official_responsibilities() {
+		return e_official_responsibilities;
+	}
+
+	public void setE_official_responsibilities(String e_official_responsibilities) {
+		this.e_official_responsibilities = e_official_responsibilities;
+	}
+
+	public int getE_total_vacation() {
+		return e_total_vacation;
+	}
+
+	public void setE_total_vacation(int e_total_vacation) {
+		this.e_total_vacation = e_total_vacation;
+	}
+
+	public String getE_serving() {
+		return e_serving;
+	}
+
+	public void setE_serving(String e_serving) {
+		this.e_serving = e_serving;
+	}
+
+	public String getD_name() {
+		return d_name;
+	}
+
+	public void setD_name(String d_name) {
+		this.d_name = d_name;
+	}
+	
+	
+	
+}
