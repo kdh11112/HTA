63aa646dc443d891c59618906913f1a7bd4d0fcc
YG <djw2717@naver.com>
2023-05-16 15:02:03 +0900

    yu commit

--- src/main/java/vo/AttendenceVO.java
+++ src/main/java/vo/AttendenceVO.java
@@ -1,5 +1,12 @@
 package vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AttendenceVO {
 int ATTENDANCE_NO;
 String WORKING_DATE;
@@ -7,3 +14,6 @@ String OFFICE_GOING_HOUR;
 String QUITTING_TIME;
 String ATTENDANCE_STATUS;
 }
+
+
+//ATTENDANCE_SEQ -- 시퀀스
