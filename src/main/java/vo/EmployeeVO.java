1754576ccf5f79ade8b2f5b8e892f2a4bdb19eff
HJ <heeee20000@gmail.com>
2023-05-17 12:03:27 +0900

    23/05/17 dao,vo edit commit -hj

--- src/main/java/vo/EmployeeVO.java
+++ src/main/java/vo/EmployeeVO.java
@@ -1,187 +1,188 @@
 package vo;
 
 public class EmployeeVO {
-	private int e_number;
-	private String e_name;
-	private String e_id;
-	private String e_password;
-	private String e_phone_number;
-	private int e_postal_code;
-	private String e_address1;
-	private String e_address2;
-	private String e_birth;
-	private String e_date_joining_company;
-	private String e_retirement_date;
-	private String e_account_information;
-	private String e_gender;
-	private String e_official_responsibilities;
-	private int e_total_vacation;
-	private String e_serving;
-	private String d_name;
+	private int eNumber;
+	private String eName;
+	private String eId;
+	private String ePassword;
+	private String ePhonenumber;
+	private int ePostalCode;
+	private String eAddress1;
+	private String eAddress2;
+	private String eBirth;
+	private String eDateJoiningCompany;
+	private String eRetirementDate;
+	private String eAccountInformation;
+	private String eGender;
+	private String eOfficialResponsibilities;
+	private int eTotalVacation;
+	private String eServing;
+	private String dName;
 	
 	public EmployeeVO() {
 		
 	}
-
-	public EmployeeVO(int e_number, String e_name, String e_id, String e_password, String e_phone_number,
-			int e_postal_code, String e_address1, String e_address2, String e_birth, String e_date_joining_company,
-			String e_retirement_date, String e_account_information, String e_gender, String e_official_responsibilities,
-			int e_total_vacation, String e_serving, String d_name) {
+	
+	public EmployeeVO(int eNumber, String eName, String eId, String ePassword, String ePhonenumber, int ePostalCode,
+			String eAddress1, String eAddress2, String eBirth, String eDateJoiningCompany, String eRetirementDate,
+			String eAccountInformation, String eGender, String eOfficialResponsibilities, int eTotalVacation,
+			String eServing, String dName) {
 		super();
-		this.e_number = e_number;
-		this.e_name = e_name;
-		this.e_id = e_id;
-		this.e_password = e_password;
-		this.e_phone_number = e_phone_number;
-		this.e_postal_code = e_postal_code;
-		this.e_address1 = e_address1;
-		this.e_address2 = e_address2;
-		this.e_birth = e_birth;
-		this.e_date_joining_company = e_date_joining_company;
-		this.e_retirement_date = e_retirement_date;
-		this.e_account_information = e_account_information;
-		this.e_gender = e_gender;
-		this.e_official_responsibilities = e_official_responsibilities;
-		this.e_total_vacation = e_total_vacation;
-		this.e_serving = e_serving;
-		this.d_name = d_name;
+		this.eNumber = eNumber;
+		this.eName = eName;
+		this.eId = eId;
+		this.ePassword = ePassword;
+		this.ePhonenumber = ePhonenumber;
+		this.ePostalCode = ePostalCode;
+		this.eAddress1 = eAddress1;
+		this.eAddress2 = eAddress2;
+		this.eBirth = eBirth;
+		this.eDateJoiningCompany = eDateJoiningCompany;
+		this.eRetirementDate = eRetirementDate;
+		this.eAccountInformation = eAccountInformation;
+		this.eGender = eGender;
+		this.eOfficialResponsibilities = eOfficialResponsibilities;
+		this.eTotalVacation = eTotalVacation;
+		this.eServing = eServing;
+		this.dName = dName;
 	}
 
-	public int getE_number() {
-		return e_number;
+	public int geteNumber() {
+		return eNumber;
 	}
 
-	public void setE_number(int e_number) {
-		this.e_number = e_number;
+	public void seteNumber(int eNumber) {
+		this.eNumber = eNumber;
 	}
 
-	public String getE_name() {
-		return e_name;
+	public String geteName() {
+		return eName;
 	}
 
-	public void setE_name(String e_name) {
-		this.e_name = e_name;
+	public void seteName(String eName) {
+		this.eName = eName;
 	}
 
-	public String getE_id() {
-		return e_id;
+	public String geteId() {
+		return eId;
 	}
 
-	public void setE_id(String e_id) {
-		this.e_id = e_id;
+	public void seteId(String eId) {
+		this.eId = eId;
 	}
 
-	public String getE_password() {
-		return e_password;
+	public String getePassword() {
+		return ePassword;
 	}
 
-	public void setE_password(String e_password) {
-		this.e_password = e_password;
+	public void setePassword(String ePassword) {
+		this.ePassword = ePassword;
 	}
 
-	public String getE_phone_number() {
-		return e_phone_number;
+	public String getePhonenumber() {
+		return ePhonenumber;
 	}
 
-	public void setE_phone_number(String e_phone_number) {
-		this.e_phone_number = e_phone_number;
+	public void setePhonenumber(String ePhonenumber) {
+		this.ePhonenumber = ePhonenumber;
 	}
 
-	public int getE_postal_code() {
-		return e_postal_code;
+	public int getePostalCode() {
+		return ePostalCode;
 	}
 
-	public void setE_postal_code(int e_postal_code) {
-		this.e_postal_code = e_postal_code;
+	public void setePostalCode(int ePostalCode) {
+		this.ePostalCode = ePostalCode;
 	}
 
-	public String getE_address1() {
-		return e_address1;
+	public String geteAddress1() {
+		return eAddress1;
 	}
 
-	public void setE_address1(String e_address1) {
-		this.e_address1 = e_address1;
+	public void seteAddress1(String eAddress1) {
+		this.eAddress1 = eAddress1;
 	}
 
-	public String getE_address2() {
-		return e_address2;
+	public String geteAddress2() {
+		return eAddress2;
 	}
 
-	public void setE_address2(String e_address2) {
-		this.e_address2 = e_address2;
+	public void seteAddress2(String eAddress2) {
+		this.eAddress2 = eAddress2;
 	}
 
-	public String getE_birth() {
-		return e_birth;
+	public String geteBirth() {
+		return eBirth;
 	}
 
-	public void setE_birth(String e_birth) {
-		this.e_birth = e_birth;
+	public void seteBirth(String eBirth) {
+		this.eBirth = eBirth;
 	}
 
-	public String getE_date_joining_company() {
-		return e_date_joining_company;
+	public String geteDateJoiningCompany() {
+		return eDateJoiningCompany;
 	}
 
-	public void setE_date_joining_company(String e_date_joining_company) {
-		this.e_date_joining_company = e_date_joining_company;
+	public void seteDateJoiningCompany(String eDateJoiningCompany) {
+		this.eDateJoiningCompany = eDateJoiningCompany;
 	}
 
-	public String getE_retirement_date() {
-		return e_retirement_date;
+	public String geteRetirementDate() {
+		return eRetirementDate;
 	}
 
-	public void setE_retirement_date(String e_retirement_date) {
-		this.e_retirement_date = e_retirement_date;
+	public void seteRetirementDate(String eRetirementDate) {
+		this.eRetirementDate = eRetirementDate;
 	}
 
-	public String getE_account_information() {
-		return e_account_information;
+	public String geteAccountInformation() {
+		return eAccountInformation;
 	}
 
-	public void setE_account_information(String e_account_information) {
-		this.e_account_information = e_account_information;
+	public void seteAccountInformation(String eAccountInformation) {
+		this.eAccountInformation = eAccountInformation;
 	}
 
-	public String getE_gender() {
-		return e_gender;
+	public String geteGender() {
+		return eGender;
 	}
 
-	public void setE_gender(String e_gender) {
-		this.e_gender = e_gender;
+	public void seteGender(String eGender) {
+		this.eGender = eGender;
 	}
 
-	public String getE_official_responsibilities() {
-		return e_official_responsibilities;
+	public String geteOfficialResponsibilities() {
+		return eOfficialResponsibilities;
 	}
 
-	public void setE_official_responsibilities(String e_official_responsibilities) {
-		this.e_official_responsibilities = e_official_responsibilities;
+	public void seteOfficialResponsibilities(String eOfficialResponsibilities) {
+		this.eOfficialResponsibilities = eOfficialResponsibilities;
 	}
 
-	public int getE_total_vacation() {
-		return e_total_vacation;
+	public int geteTotalVacation() {
+		return eTotalVacation;
 	}
 
-	public void setE_total_vacation(int e_total_vacation) {
-		this.e_total_vacation = e_total_vacation;
+	public void seteTotalVacation(int eTotalVacation) {
+		this.eTotalVacation = eTotalVacation;
 	}
 
-	public String getE_serving() {
-		return e_serving;
+	public String geteServing() {
+		return eServing;
 	}
 
-	public void setE_serving(String e_serving) {
-		this.e_serving = e_serving;
+	public void seteServing(String eServing) {
+		this.eServing = eServing;
 	}
 
-	public String getD_name() {
-		return d_name;
+	public String getdName() {
+		return dName;
 	}
 
-	public void setD_name(String d_name) {
-		this.d_name = d_name;
+	public void setdName(String dName) {
+		this.dName = dName;
 	}
+
 	
 	
 	
